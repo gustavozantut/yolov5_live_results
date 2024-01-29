ff6b7143051a921534f92bd204b36e7c334f358f
Gustavo Abbomerato Zantut <gustavo_Zantut@hotmail.com>
2024-01-29 11:23:54 -0300

    Update plots.py
    Create function that saves one frame.
    
    Signed-off-by: Gustavo Abbomerato Zantut <gustavo_Zantut@hotmail.com>

--- utils/plots.py
+++ utils/plots.py
@@ -470,3 +470,12 @@ def save_one_box(xyxy, im, file=Path("im.jpg"), gain=1.02, pad=10, square=False,
         # cv2.imwrite(f, crop)  # save BGR, https://github.com/ultralytics/yolov5/issues/7007 chroma subsampling issue
         Image.fromarray(crop[..., ::-1]).save(f, quality=95, subsampling=0)  # save RGB
     return crop
+
+
+def save_one_frame(im, file=Path("im.png")):
+    # Save frame
+    file.parent.mkdir(parents=True, exist_ok=True)  # make directory
+    f = str(increment_path(file).with_suffix(".png"))
+    # cv2.imwrite(f, crop)  # save BGR, https://github.com/ultralytics/yolov5/issues/7007 chroma subsampling issue
+    Image.fromarray(im[..., ::-1]).save(f, quality=95, subsampling=0)  # save RGB
+    return im
